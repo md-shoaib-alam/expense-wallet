8e6ef7b8241c0b6233f424b7ea71c7c0c26ccfb3
md-shoaib-alam <shoaibsalam0786@gmail.com>
2025-08-14 22:10:55 +0530

    .

--- mobile/constants/colors.js
+++ mobile/constants/colors.js
@@ -484,4 +484,4 @@ export const THEMES = {
 
 
 // 👇 change this to switch theme
-export const COLORS = THEMES.mint; // Change this to switch theme
+export const COLORS = THEMES.coffee; // Change this to switch theme
